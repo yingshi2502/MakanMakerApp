b3669020d25e113111b475e50bf218cd4096d989
Ismahfaris <ismahfaris@gmail.com>
2018-04-21 20:15:20 +0800

    Add to Shopping Cart Realised

--- src/pages/view-all-meal-kits/view-all-meal-kits.ts
+++ src/pages/view-all-meal-kits/view-all-meal-kits.ts
@@ -3,6 +3,7 @@ import { NavController, NavParams } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 import {ViewMealKitDetailsPage} from '../view-meal-kit-details/view-meal-kit-details';
 import { MealKitProvider } from '../../providers/meal-kit/meal-kit';
+import { ShoppingCartProvider } from '../../providers/shopping-cart/shopping-cart';
 import { MealKit } from '../../entities/mealKit';
 /**
  * Generated class for the ViewAllMealKitsPage page.
@@ -16,12 +17,26 @@ import { MealKit } from '../../entities/mealKit';
   templateUrl: 'view-all-meal-kits.html',
 })
 export class ViewAllMealKitsPage {
+	infoMessage: string;
 	errorMessage: string;
 	mealKits: MealKit[];
+	qty:string="";
+	customerId: number;
+	customerIdString: string;
+	mealKitIdInString: string='';
 	
-
-  constructor(public navCtrl: NavController, public navParams: NavParams,public alertCtrl: AlertController
-  , public mealKitProvider: MealKitProvider) {
+	
+  constructor(public navCtrl: NavController, 
+				public navParams: NavParams,
+				public alertCtrl: AlertController,
+				public mealKitProvider: MealKitProvider,
+				public shoppingCartProvider: ShoppingCartProvider)
+ {
+	
+		let customerIdInString: string = sessionStorage.getItem("customerId");
+		this.customerIdString = customerIdInString; //this will be used for addItem
+		this.customerId = Number(customerIdInString);
+ 
   }
 
   
@@ -49,9 +64,22 @@ export class ViewAllMealKitsPage {
 			}
 		);
 	}
-
   
-    doAlert() {
+  
+  doAlert(mealKit) {
+	this.mealKitIdInString = '' + mealKit.mealKitId.toString();  
+	this.shoppingCartProvider.addItem(this.customerIdString, this.mealKitIdInString, "1").subscribe(
+		response => {						
+			this.infoMessage = "Added to shopping cart successfully";
+			this.errorMessage = null;
+		},
+		error => {				
+			this.infoMessage = null;
+			this.errorMessage = "HTTP " + error.status + ": " + error.error.message;
+		}
+	);	  
+	  
+	  
     let alert = this.alertCtrl.create({
       title: 'Added to Cart!',
       subTitle: 'Item has been added to Cart successfully!',
